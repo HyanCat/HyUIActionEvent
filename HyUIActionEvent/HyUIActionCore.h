8e781212ad92dfda9899aeb05abf0df0aead17c7
HyanCat <hyancat@live.cn>
2016-03-30 21:45:54 +0800

    update.

--- HyUIActionEvent/HyUIActionCore.h
+++ HyUIActionEvent/HyUIActionCore.h
@@ -1,17 +1,16 @@
 //
-//  HyUIActionEventCore.h
-//  Pods
+//  HyUIActionCore.h
 //
 //  Created by HyanCat on 16/3/30.
-//
+//  Copyright © 2016年 hyancat. All rights reserved.
 //
 
-#ifndef HyUIActionEventCore_h
-#define HyUIActionEventCore_h
+#ifndef HyUIActionCore_h
+#define HyUIActionCore_h
 
 #import <HyUIActionEvent/HyUIActionEvent.h>
 #import <HyUIActionEvent/HyUIActionEventResult.h>
 #import <HyUIActionEvent/UIResponder+HyUIActionEvent.h>
 #import <HyUIActionEvent/HyUIActionEvent+Transition.h>
 
-#endif /* HyUIActionEventCore_h */
+#endif /* HyUIActionCore_h */
