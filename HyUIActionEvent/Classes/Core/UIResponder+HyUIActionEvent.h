867a167262f162c38b640402844a1e93f2c6cd50
HyanCat <hyancat.cn@gmail.com>
2018-07-13 19:12:08 +0800

    Fix typo.

--- HyUIActionEvent/Classes/Core/UIResponder+HyUIActionEvent.h
+++ HyUIActionEvent/Classes/Core/UIResponder+HyUIActionEvent.h
@@ -16,7 +16,7 @@ NS_ASSUME_NONNULL_BEGIN
  * 分发界面事件
  *
  * 想要处理该事件的对象，需要实现形如：
- *  - (HyUIActionEventHandleResult *)handle%@WithActionEvent:(HyUIActionEvent *)actionEvent;
+ *  - (HyUIActionEventResult *)handle%@WithActionEvent:(HyUIActionEvent *)actionEvent;
  *
  * %@ 为事件名，首字母将自动转为大写
  * 返回对象为 nil, 或者返回对象的 continueDispatch 属性为 NO 时，表示该事件处理结束，不再传递该事件
