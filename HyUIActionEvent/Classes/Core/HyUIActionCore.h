1cd7f4703e8cceeee4b4c467e5e3f5ef1c1f6af7
HyanCat <hyancat.cn@gmail.com>
2019-11-27 18:46:42 +0800

    Add macros.

--- HyUIActionEvent/Classes/Core/HyUIActionCore.h
+++ HyUIActionEvent/Classes/Core/HyUIActionCore.h
@@ -14,4 +14,40 @@
 #import <HyUIActionEvent/UIControl+HyUIActionEvent.h>
 #import <HyUIActionEvent/UIView+HyUIActionEvent.h>
 
+/**
+ We have two ways to handle the event easily.
+
+ First way is like:
+
+ ```ObjectiveC
+ HyUIActionHandler(ClickEventA, {
+    // Your code here...
+    return NO;
+ })
+ ```
+
+ Another way is like:
+
+ ```ObjectiveC
+ HyUIActionHandlerBegin(ClickEventA) {
+    // Your code here...
+    HyUIActionHandlerEnd(NO)
+ }
+ ```
+ */
+
+#define HyUIActionHandler(name, block) \
+- (HyUIActionEventResult *)handle##name##WithActionEvent:(HyUIActionEvent *)event { \
+    BOOL (^handlerBlock)(HyUIActionEvent *) = ^(HyUIActionEvent *event) block; \
+    BOOL continueDispatching = handlerBlock(event); \
+    return [HyUIActionEventResult resultWithContinueDispatching:continueDispatching]; \
+}
+
+
+#define HyUIActionHandlerBegin(name) \
+- (HyUIActionEventResult *)handle##name##WithActionEvent:(HyUIActionEvent *)event
+
+#define HyUIActionHandlerEnd(continue) \
+return [HyUIActionEventResult resultWithContinueDispatching:continue];
+
 #endif /* HyUIActionCore_h */
